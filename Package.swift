1369ddb1bae093b443b3f4075342abf68b385191
Sergey Zenchenko <sergeizenchenko@gmail.com>
2019-06-08 13:01:54 +0300

    Update name

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "msgpack-swift",
+    name: "MessagePackSwift",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "msgpack-swift",
+            name: "MessagePackSwift",
             targets: ["MsgPack"]),
     ],
     dependencies: [
